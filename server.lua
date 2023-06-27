8311dc60b5952c1c6d9531012be55137c939577b
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2023-06-27 22:10:30 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,17 +1,17 @@
 ESX = nil
 UseNewESX = true -- true / false
 
-
 if not UseNewESX then
 	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 else
 	ESX = exports["es_extended"]:getSharedObject()
 end
 
+-------- Item ------------
 ESX.RegisterUsableItem(Config.BatteryItem, function(playerid)
     local xPlayer = ESX.GetPlayerFromId(playerid)
-	xPlayer.removeInventoryItem(Config.BatteryItem, 1)
-		local batteryamount = 100
-		TriggerClientEvent('mfp_phonebattery:setBattery', xPlayer.source, batteryamount)
+	xPlayer.removeInventoryItem("phone_battery", 1)
+		local battery = 100
+		TriggerClientEvent('lb-addon-phone-community:setBattery', xPlayer.source, battery)
 		TriggerClientEvent('esx:showNotification', xPlayer.source, "Phone charged 100% again!")
 end)
